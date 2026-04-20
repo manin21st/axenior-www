1920cf76f75357b73bde145bad6d68d58b6c5932
bhsong <manin21st@gmail.com>
2026-04-20 13:15:57 +0900

    feat: 메인 페이지 조립 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import { Hero } from '@/components/sections/Hero'
+import { About } from '@/components/sections/About'
+import { Services } from '@/components/sections/Services'
+import { Portfolio } from '@/components/sections/Portfolio'
+import { ContactSection } from '@/components/sections/ContactSection'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
+      <Services />
+      <Portfolio />
+      <ContactSection />
     </main>
   )
 }
